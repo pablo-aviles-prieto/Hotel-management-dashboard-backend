33ad962221f096c0c35f7a98bfaddd8fb5a4d0ba
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2023-01-09 23:28:02 +0100

    refactorize error logic on users controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,43 +1,71 @@
-import { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
 import { hashSync } from 'bcryptjs';
 import { UserModel } from '../models';
+import { ControllerError } from '../errors';
+import { Request, Response, NextFunction } from 'express';
 
 const { BCRYPT_SALT } = process.env;
 
 export const getUsersList = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const usersList = await UserModel.find().exec();
+    if (usersList.length === 0) {
+      next(new ControllerError({ name: 'Error users list', message: `Couldn't find any user`, status: 404 }));
+      return;
+    }
     res.status(200).json({ result: usersList });
   } catch (error) {
-    next(error);
+    if (error instanceof mongoose.Error) {
+      next(
+        new ControllerError({
+          name: 'Error users list',
+          message: 'Error getting the users list on Mongo',
+          status: 400,
+          additionalMessage: error.message
+        })
+      );
+      return;
+    }
+    next(new ControllerError({ name: 'Error users list', message: 'Error getting the users list', status: 500 }));
   }
 };
 
 export const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { photo, name, email, password, startDate, job, contact, status } = req.body;
 
-  const existingUserByEmail = await UserModel.findOne({ email }).exec();
-  if (existingUserByEmail) return res.status(400).json({ result: 'Error creating the user' });
-
-  const salt = BCRYPT_SALT ? Number(BCRYPT_SALT) : 12;
-  const hashedPassword = hashSync(password, salt);
-
-  const user = new UserModel({
-    photo,
-    name,
-    email,
-    password: hashedPassword,
-    startDate,
-    job,
-    contact,
-    status
-  });
-
   try {
+    const existingUserByEmail = await UserModel.findOne({ email }).exec();
+    if (existingUserByEmail) return res.status(400).json({ result: 'Error creating the user' });
+
+    const salt = BCRYPT_SALT ? Number(BCRYPT_SALT) : 12;
+    const hashedPassword = hashSync(password, salt);
+
+    const user = new UserModel({
+      photo,
+      name,
+      email,
+      password: hashedPassword,
+      startDate,
+      job,
+      contact,
+      status
+    });
+
     const result = await user.save();
     res.status(201).json({ result });
   } catch (error) {
-    next(error);
+    if (error instanceof mongoose.Error) {
+      next(
+        new ControllerError({
+          name: 'Error creating user',
+          message: 'Error creating the user on Mongo',
+          status: 400,
+          additionalMessage: error.message
+        })
+      );
+      return;
+    }
+    next(new ControllerError({ name: 'Error creating user', message: 'Error creating the user', status: 500 }));
   }
 };
 
@@ -46,10 +74,30 @@ export const getSingleUser = async (req: Request, res: Response, next: NextFunct
 
   try {
     const user = await UserModel.findById(userId).exec();
-    if (!user) return res.status(400).json({ result: 'Error fetching the user' });
+    if (!user) {
+      next(
+        new ControllerError({
+          name: 'Error single user',
+          message: `Couldn't find the selected user`,
+          status: 404
+        })
+      );
+      return;
+    }
     res.status(200).json({ result: user });
   } catch (error) {
-    next(error);
+    if (error instanceof mongoose.Error) {
+      next(
+        new ControllerError({
+          name: 'Error single user',
+          message: 'Error getting the user on Mongo',
+          status: 400,
+          additionalMessage: error.message
+        })
+      );
+      return;
+    }
+    next(new ControllerError({ name: 'Error single user', message: 'Error getting the user', status: 500 }));
   }
 };
 
@@ -59,7 +107,16 @@ export const editUser = async (req: Request, res: Response, next: NextFunction) 
 
   try {
     const existUser = await UserModel.findById(userId).exec();
-    if (!existUser) return res.status(400).send({ result: 'Error fetching the user' });
+    if (!existUser) {
+      next(
+        new ControllerError({
+          name: 'Error editing user',
+          message: `Couldn't find the selected user`,
+          status: 404
+        })
+      );
+      return;
+    }
 
     for (const property in req.body) {
       if (property === 'job') {
@@ -83,7 +140,18 @@ export const editUser = async (req: Request, res: Response, next: NextFunction) 
 
     res.status(202).json({ result: existUser });
   } catch (error) {
-    next(error);
+    if (error instanceof mongoose.Error) {
+      next(
+        new ControllerError({
+          name: 'Error editing user',
+          message: 'Error editing the user on Mongo',
+          status: 400,
+          additionalMessage: error.message
+        })
+      );
+      return;
+    }
+    next(new ControllerError({ name: 'Error editing user', message: 'Error editing the user', status: 500 }));
   }
 };
 
@@ -92,12 +160,32 @@ export const deleteUser = async (req: Request, res: Response, next: NextFunction
 
   try {
     const existUser = await UserModel.findById(userId).exec();
-    if (!existUser) return res.status(400).send({ result: 'Error deleting the user' });
+    if (!existUser) {
+      next(
+        new ControllerError({
+          name: 'Error deleting user',
+          message: `Couldn't find the selected user`,
+          status: 400
+        })
+      );
+      return;
+    }
 
     await existUser.delete();
 
     res.status(202).json({ result: 'User deleted successfully' });
   } catch (error) {
-    next(error);
+    if (error instanceof mongoose.Error) {
+      next(
+        new ControllerError({
+          name: 'Error deleting user',
+          message: 'Error deleting the user on Mongo',
+          status: 400,
+          additionalMessage: error.message
+        })
+      );
+      return;
+    }
+    next(new ControllerError({ name: 'Error deleting user', message: 'Error deleting the user', status: 500 }));
   }
 };
